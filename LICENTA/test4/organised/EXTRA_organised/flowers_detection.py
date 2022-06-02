b7bfe71ae35f2ec293f34518d0d5632e93c212d9
tudorhutu <tudorhutu2012@gmail.com>
2022-06-02 23:33:28 +0300

    jhhj

--- LICENTA/test4/organised/EXTRA_organised/flowers_detection.py
+++ LICENTA/test4/organised/EXTRA_organised/flowers_detection.py
@@ -32,68 +32,83 @@ class_names = train_ds.class_names
 
 
 
-model = tf.keras.models.load_model("./mega_model_saved")
-sunflower_url = "https://i.guim.co.uk/img/media/3a9f56c9aeed31f4b87aae361bca1f37dda723ff/247_0_4857_2916/master/4857.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=f0386f1da4e055665fec47acc62ad30a"
-sunflower_path = tf.keras.utils.get_file('babuiasdn', origin=sunflower_url)
+# model = tf.keras.models.load_model("./mega_model_saved")
+# sunflower_url = "https://www.animenewsnetwork.com/thumbnails/crop1200x630gRB/cms/the-list/101603/monkeygintama.jpg"
+# sunflower_path = tf.keras.utils.get_file('gintamag', origin=sunflower_url)
 
-img = tf.keras.utils.load_img(
-    sunflower_path, target_size=(img_height, img_width)
-)
-img_array1 = tf.keras.utils.img_to_array(img)
-img_array1 = tf.expand_dims(img_array1, 0) # Create a batch
-
-
-sunflower_url = "https://upload.wikimedia.org/wikipedia/commons/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg"
-sunflower_path = tf.keras.utils.get_file('coasdasdmo', origin=sunflower_url)
-
-img = tf.keras.utils.load_img(
-    sunflower_path, target_size=(img_height, img_width)
-)
-img_array2 = tf.keras.utils.img_to_array(img)
-img_array2 = tf.expand_dims(img_array2, 0) # Create a batch
+# img = tf.keras.utils.load_img(
+#     sunflower_path, target_size=(img_height, img_width)
+# )
+# img_array1 = tf.keras.utils.img_to_array(img)
+# img_array1 = tf.expand_dims(img_array1, 0) # Create a batch
 
 
+# sunflower_url = "https://upload.wikimedia.org/wikipedia/commons/2/20/Saimiri_sciureus-1_Luc_Viatour.jpg"
+# sunflower_path = tf.keras.utils.get_file('coasdasdmo', origin=sunflower_url)
+
+# img = tf.keras.utils.load_img(
+#     sunflower_path, target_size=(img_height, img_width)
+# )
+# img_array2 = tf.keras.utils.img_to_array(img)
+# img_array2 = tf.expand_dims(img_array2, 0) # Create a batch
 
 
 
-#
-
-model = tf.keras.models.load_model("./mega_model_saved")
-# sunflower_url = "https://cache.desktopnexus.com/thumbseg/2352/2352605-bigthumbnail.jpg"
-# sunflower_path = tf.keras.utils.get_file('bouquet', origin=sunflower_url)
-path_2="C:\\Users\\Tudor\\Desktop\\rosesandtulips.jpg"
-
-img = tf.keras.utils.load_img(
-    path_2, target_size=(img_height, img_width)
-)
-img_array3 = tf.keras.utils.img_to_array(img)
-img_array3 = tf.expand_dims(img_array3, 0) # Create a batch
 
 
+# #
 
-if __name__ == "main":
-    predictions = model.predict(img_array1)
-    score = tf.nn.softmax(predictions[0])
-    print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
+# model = tf.keras.models.load_model("./mega_model_saved")
+# # sunflower_url = "https://cache.desktopnexus.com/thumbseg/2352/2352605-bigthumbnail.jpg"
+# # sunflower_path = tf.keras.utils.get_file('bouquet', origin=sunflower_url)
+# path_2="C:\\Users\\Tudor\\Desktop\\rosesandtulips.jpg"
+
+# img = tf.keras.utils.load_img(
+#     path_2, target_size=(img_height, img_width)
+# )
+# img_array3 = tf.keras.utils.img_to_array(img)
+# img_array3 = tf.expand_dims(img_array3, 0) # Create a batch
+
+
+def pretdict(path_to_predict):
+    model = tf.keras.models.load_model("./mega_model_saved_2")
+    image = tf.keras.utils.load_img(
+    path_to_predict, target_size=(img_height, img_width)
     )
-
-
-    predictions = model.predict(img_array2)
+    image_array = tf.keras.utils.img_to_array(image)
+    image_array = tf.expand_dims(image_array, 0)
+    predictions = model.predict(image_array)
     score = tf.nn.softmax(predictions[0])
+    final_class = class_names[np.argmax(score)]
+    final_score = 100 * np.max(score)
+    return final_class
 
-    print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
+# if __name__ == "main":
+# predictions = model.predict(img_array1)
+# score = tf.nn.softmax(predictions[0])
+# final_class = class_names[np.argmax(score)]
+# final_score = 100 * np.max(score)
+# print(class_names)
+# print(predictions)
+# print(
+#     "This image most likely belongs to {} with a {:.2f} percent confidence."
+#     .format(class_names[np.argmax(score)], 100 * np.max(score))
+# )
 
 
-    predictions = model.predict(img_array3)
-    score = tf.nn.softmax(predictions[0])
-    print(predictions)
-    print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
+# predictions = model.predict(img_array2)
+# score = tf.nn.softmax(predictions[0])
+
+# print(
+#     "This image most likely belongs to {} with a {:.2f} percent confidence."
+#     .format(class_names[np.argmax(score)], 100 * np.max(score))
+# )
+
+
+# predictions = model.predict(img_array3)
+# score = tf.nn.softmax(predictions[0])
+# print(
+#     "This image most likely belongs to {} with a {:.2f} percent confidence."
+#     .format(class_names[np.argmax(score)], 100 * np.max(score))
+# )
 
